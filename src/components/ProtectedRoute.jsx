06cfa248bc2f944124567e53e27c877065222226
Rroon3y <rooneyalex2016@gmail.com>
2025-11-03 01:41:29 +0300

    added import for useAuth

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 // src/components/ProtectedRoute.jsx
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../hooks/useAuth";
 
 export default function ProtectedRoute({ role }) {
   const { user } = useAuth();
