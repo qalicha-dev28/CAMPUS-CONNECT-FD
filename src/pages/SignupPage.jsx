299061f5b07d268279d0e4caeda53461089b14e5
Rroon3y <rooneyalex2016@gmail.com>
2025-11-03 00:50:33 +0300

    add

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../hooks/useAuth";
 import { motion } from "framer-motion";
 
 export default function SignupPage() {
