397a1f33883695e7efe078f9dc39c1a55f45872e
Rroon3y <rooneyalex2016@gmail.com>
2025-11-04 10:20:02 +0300

    resolved ports issues

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -24,7 +24,7 @@ export default function SignupPage() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/signup", {
+      const response = await fetch("http://localhost:5000/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
